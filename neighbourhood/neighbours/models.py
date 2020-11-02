6ae022bc4d6d639ff8bc71087b5b82a5f84f1a2e
gracewa <33077294+gracewa@users.noreply.github.com>
2020-11-02 07:35:18 +0300

    Models

--- neighbourhood/neighbours/models.py
+++ neighbourhood/neighbours/models.py
@@ -1,3 +1,57 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
-# Create your models here.
+class Neighbourhood(models.Model):
+    name = models.CharField(max_length=100)
+    location = models.CharField(max_length=100)
+    admin = models.ForeignKey('User', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+    def save_neighbourhood(self):
+        self.save()
+
+    @classmethod
+    def delete_neighbourhood(cls,name):
+        cls.objects.filter(name=name).delete()
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    name = models.TextField(max_length=100, blank=True)
+    neighbourhood = models.ForeignKey(Neighbourhood, on_delete=models.CASCADE)
+    status = models.CharField(max_length=100)
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()
+
+class Business(models.Model):
+    name =models.CharField(max_length=100)
+    description = models.TextField()
+    email = models.EmailField()
+    location =models.CharField(max_length=100)
+    phone = models.IntegerField()
+    owner = models.ForeignKey(User,on_delete=models.CASCADE)
+    neighbourhood = models.ForeignKey(Neighbourhood, on_delete=models.CASCADE)
+
+
+    def __str__(self):
+        return self.name
+class EmergencyService(models.Model):
+    type = models.CharField(max_length=100)
+    name =models.CharField(max_length=100)
+    description = models.TextField()
+    location =models.CharField(max_length=100)
+    phone = models.IntegerField()
+    neighbourhood = models.ForeignKey(Neighbourhood, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
