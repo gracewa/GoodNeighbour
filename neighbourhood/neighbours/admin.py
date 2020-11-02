aad6017f17bb182b777dcbd1f3c27c1f341ee61c
gracewa <33077294+gracewa@users.noreply.github.com>
2020-11-02 07:39:47 +0300

    registering models and creating super user

--- neighbourhood/neighbours/admin.py
+++ neighbourhood/neighbours/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Neighbourhood, Business, EmergencyService, Profile
+
+
+admin.site.register(Neighbourhood)
+admin.site.register(Business)
+admin.site.register(EmergencyService)
+admin.site.register(Profile)
